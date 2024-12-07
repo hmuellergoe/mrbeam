97192f57b1e12d9b574d752f6fc966f3715fa77c
Hendrik Muller <hmuller@smurf.aoc.nrao.edu>
2024-12-07 10:37:19 -0700

    change time entropy gradient

--- mr_beam/imagingbase/imagingbase/ngMEM_functional.py
+++ mr_beam/imagingbase/imagingbase/ngMEM_functional.py
@@ -29,7 +29,7 @@ class MovieEntropy(Functional):
         for i in range(self.nr_of_frames):
             toret += self.functionals[i].gradient(_imvecs[i])
         return toret
-    
+
 class TemporalEntropy(Functional):
     def __init__(self, domain, nr_of_frames, C, tau, **kwargs):
         self.nr_of_frames = nr_of_frames
@@ -52,16 +52,15 @@ class TemporalEntropy(Functional):
                 self.imvecdiff[:,i,j] = np.abs(_imvecs[i].flatten()-_imvecs[j].flatten())
         
         return np.sum(np.exp(-self.tdiff**2/(2*self.tau**2))*self.imvecdiff*np.log(self.imvecdiff+self.C))
-        
+           
     def _gradient(self, imvecs):
         _imvecs = self.domain.split(imvecs)
         for i in range(self.nr_of_frames):
             for j in range(self.nr_of_frames):
-                self.imvecdiff[:,i,j] = np.abs(_imvecs[i].flatten()-_imvecs[j].flatten())
-                
-        return np.sum(np.exp(-self.tdiff**2/(2*self.tau**2))*(np.log(self.imvecdiff+self.C)+self.imvecdiff/(self.imvecdiff+self.C)), axis=1).flatten() \
-                    + np.sum(np.exp(-self.tdiff**2/(2*self.tau**2))*(np.log(self.imvecdiff+self.C)+self.imvecdiff/(self.imvecdiff+self.C)), axis=2).flatten()
-    
+                self.imvecdiff[:,i,j] = (_imvecs[i]-_imvecs[j]).flatten()
+        signs = np.sign(self.imvecdiff)
+        self.imvecdiff = np.abs(self.imvecdiff)
+        return -np.sum(np.exp(-self.tdiff**2/(2*self.tau**2))*(signs*np.log(self.imvecdiff+self.C)+signs*self.imvecdiff/(self.imvecdiff+self.C)), axis=1).transpose().flatten()
     
     
     
